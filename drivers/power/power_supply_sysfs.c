5f9d2a1f4f0a833356e0060f5d357957e1664981
hongwei.di <hongwei.di@archermind.com>
2019-09-11 18:59:25 +0800

    [ZE620KL-Q][ARA][Charger]: Add userspace charging status context
    Change-Id: Icaa8e722c484bf3228f000d668f0d59ffe74fbc0
    Signed-off-by: hongwei.di <hongwei.di@archermind.com>

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -49,8 +49,10 @@ static ssize_t power_supply_show_property(struct device *dev,
 		"Wireless", "USB_FLOAT", "BMS", "Parallel", "Main", "Wipower",
 		"TYPEC", "TYPEC_UFP", "TYPEC_DFP"
 	};
+	//WeiYu add  "Thermal Alert" "10W Quick charging","Not 10W Quick charging"
 	static char *status_text[] = {
-		"Unknown", "Charging", "Discharging", "Not charging", "Full"
+		"Unknown", "Charging", "Discharging", "Not charging", "Full","Quick charging","Not Quick charging", "Thermal Alert",
+			"10W Quick charging","Not 10W Quick charging"
 	};
 	static char *charge_type[] = {
 		"Unknown", "N/A", "Trickle", "Fast",
